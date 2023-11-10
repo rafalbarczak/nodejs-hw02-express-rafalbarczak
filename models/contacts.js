7715f0d787bbb6c4ac6bbae001ecff2d5503a5a7
rafalbarczak <rafal.barczak@intmail.pl>
2023-11-10 12:33:20 +0100

    hw03 finished

--- models/contacts.js
+++ models/contacts.js
@@ -4,107 +4,88 @@ const { v4: uuidv4 } = require("uuid");
 
 const contactsPath = path.join(__dirname, "contacts.json");
 
-const listContacts = async () => {
+const Contact = require("./Contact");
+const { nextTick } = require("process");
+const { result } = require("@hapi/joi/lib/base");
+
+const listContacts = async (req, res, next) => {
   try {
-    const data = await fs.readFile(contactsPath);
-    const contacts = JSON.parse(data);
-    return contacts;
-  } catch (error) {
-    throw new Error("Unable to read contacts data");
+    const contacts = await Contact.find().lean();
+    res.json(contacts);
+  } catch (err) {
+    next(err);
   }
 };
 
-const getContactById = async (contactId) => {
+const getContactById = async (req, res, next) => {
   try {
-    const data = await fs.readFile(contactsPath);
-    const contacts = JSON.parse(data);
-    const [filteredContact] = contacts.filter(
-      (contact) => contact.id === contactId
-    );
-    if (!filteredContact) {
-      const notFoundError = new Error("Contact not found");
-      notFoundError.status = 404;
-      throw notFoundError;
-    }
-    return filteredContact;
-  } catch (error) {
-    throw new Error("Unable to retrieve contact");
-  }
-};
-
-const removeContact = async (contactId) => {
-  try {
-    const data = await fs.readFile(contactsPath);
-    const contacts = JSON.parse(data);
-    const contactIndex = contacts.findIndex(
-      (contact) => contact.id === contactId
-    );
-    if (contactIndex === -1) {
-      const notFoundError = new Error("Contact not found");
-      notFoundError.status = 404;
-      throw notFoundError;
-    }
-    const newContacts = contacts.filter((contact) => contact.id !== contactId);
-
-    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
-    return "Contact deleted successfully";
-  } catch (error) {
-    if (error.status === 404) {
-      throw error;
+    const contact = await Contact.findOne({ _id: req.params.contactId }).lean();
+    if (contact) {
+      res.json(contact);
     } else {
-      throw new Error("Unable to delete contact");
+      next();
     }
+  } catch (err) {
+    next(err);
   }
 };
 
-const addContact = async (body) => {
+const removeContact = async (req, res, next) => {
+  const { contactId } = req.params;
   try {
-    const data = await fs.readFile(contactsPath);
-    const contacts = JSON.parse(data);
-    const newContact = {
-      id: uuidv4(),
-      name: body.name,
-      email: body.email,
-      phone: body.phone,
-    };
-    contacts.push(newContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts));
-    return newContact;
-  } catch (error) {
-    throw new Error("Unable to create contact");
+    await Contact.deleteOne({ _id: contactId });
+
+    res.status(204).send();
+  } catch (err) {
+    next(err);
   }
 };
 
-const updateContact = async (contactId, body) => {
-  let newContact = null;
+const addContact = async (req, res, next) => {
+  const { name, email, phone, favorite } = req.body;
+  try {
+    const result = await Contact.create({ name, email, phone, favorite });
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const { name, email, phone, favorite } = req.body;
 
   try {
-    const { name, email, phone } = body;
-    const data = await fs.readFile(contactsPath);
-    const contacts = JSON.parse(data);
-    const filteredContact = contacts.find(
-      (contact) => contact.id === contactId
+    const contactData = { name, email, phone, favorite };
+    const result = await Contact.findOneAndUpdate(
+      { _id: contactId },
+      contactData,
+      { new: true, upsert: true, runValidators: true, strict: "throw" }
     );
-    if (filteredContact) {
-      filteredContact.name = name;
-      filteredContact.email = email;
-      filteredContact.phone = phone;
-    } else {
-      newContact = {
-        id: uuidv4(),
-        name: body.name,
-        email: body.email,
-        phone: body.phone,
-      };
-      contacts.push(newContact);
-    }
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+const updateStatusContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const { favorite } = req.body;
 
-    await fs.writeFile(contactsPath, JSON.stringify(contacts));
-    return newContact
-      ? `Contact added successfully, new contact's id: ${newContact.id}`
-      : "Contact updated succesfully";
-  } catch (error) {
-    throw new Error("Unable to update or create co]ntact");
+  try {
+    const result = await Contact.findByIdAndUpdate(
+      { _id: contactId },
+      { favorite },
+      { new: true, runValidators: true, strict: "throw" }
+    );
+    if (favorite == undefined) {
+      res.status(400).send();
+    }
+    if (!result) {
+      next();
+    }
+    res.json(result);
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -114,4 +95,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
+  updateStatusContact,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,83 +1,18 @@
 const express = require("express");
 const contacts = require("../../models/contacts.js");
-const Joi = require("@hapi/joi");
 
 const router = express.Router();
 
-const contactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.required(),
-});
+router.get("/", contacts.listContacts);
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contactList = await contacts.listContacts();
-    res.json(contactList);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.get("/:contactId", contacts.getContactById);
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await contacts.getContactById(contactId);
-    res.json(contact);
-  } catch (error) {
-    if (error.status === 404) {
-      res.status(404).json({ message: "Not found" });
-    } else {
-      res.status(500).json({ error: error.message });
-    }
-  }
-});
+router.post("/", contacts.addContact);
 
-router.post("/", async (req, res, next) => {
-  const { name, email, phone } = req.body;
-  const validationResult = contactSchema.validate({ name, email, phone });
+router.delete("/:contactId", contacts.removeContact);
 
-  if (validationResult.error) {
-    return res.status(400).json({ message: "missing required name - field" });
-  }
-  try {
-    const contact = await contacts.addContact(req.body);
-    res.status(201).json(contact);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.put("/:contactId", contacts.updateContact);
 
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const message = await contacts.removeContact(contactId);
-    res.json(message);
-  } catch (error) {
-    if (error.status === 404) {
-      res.status(404).json({ error: error.message });
-    } else {
-      res.status(500).json({ error: error.message });
-    }
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  const { name, email, phone } = req.body;
-  const validationResult = contactSchema.validate({ name, email, phone });
-
-  if (validationResult.error) {
-    return res.status(400).json({ message: "missing required name - field" });
-  }
-  try {
-    const { contactId } = req.params;
-    const message = await contacts.updateContact(contactId, req.body);
-    const statusCode = message.includes("updated") ? 200 : 201;
-
-    res.status(statusCode).json(message);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.patch("/:contactId", contacts.updateStatusContact);
 
 module.exports = router;
